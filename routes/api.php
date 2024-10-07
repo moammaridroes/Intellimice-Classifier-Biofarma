73ec63f3695efbf6f144256e5d68e4c76ce8f469
moammaridroes <miracleboypub@gmail.com>
2024-10-07 15:51:24 +0700

    api.php

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+use App\Http\Controllers\Sensor1Controller;
+use App\Http\Controllers\Sensor2Controller;
+use App\Http\Controllers\Sensor3Controller;
+
+Route::apiResource('sensor1', Sensor1Controller::class);  // Kesehatan
+Route::apiResource('sensor2', Sensor2Controller::class);  // Jenis Kelamin
+Route::apiResource('sensor3', Sensor3Controller::class);  // Berat
